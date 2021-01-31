898f9c1201cc32061cbc2f2503d4083468932383
Roger <logroger@gmail.com>
2021-01-31 14:09:18 -0300

    Start

---
+++ src/main/java/com/github/rogerlog/apicidadesbrasil/ApiCidadesBrasilApplication.java
@@ -0,0 +1,13 @@
+package com.github.rogerlog.apicidadesbrasil;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiCidadesBrasilApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiCidadesBrasilApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/rogerlog/apicidadesbrasil/ApiCidadesBrasilApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.rogerlog.apicidadesbrasil;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiCidadesBrasilApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
